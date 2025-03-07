6ddd59ff4622e362e831d16e99fcda426b2f4181
dyloge <dylogedyloge@gmail.com>
2025-03-07 09:03:05 +0330

    still unsuccessful in proxying websocket

--- utils/proxy-helper.ts
+++ utils/proxy-helper.ts
@@ -1,23 +1,41 @@
-// Create a proxy URL with authentication
-const PROXY_URL = 'http://Jungp2jf5I:866OI8O8nZ@cdn.smatrip.com:39210';
-
-// Override global fetch
-const originalFetch = global.fetch;
-global.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
-  const url = input.toString();
-  const proxyUrl = url.replace(/^https?:\/\//, `http://${PROXY_URL}/`);
-  
-  return originalFetch(proxyUrl, {
-    ...init,
-    headers: {
-      ...init?.headers,
-      'Connection': 'keep-alive',
-      'Proxy-Connection': 'keep-alive'
-    }
-  });
+// Proxy configuration
+const PROXY_CONFIG = {
+  host: "cdn.smatrip.com",
+  port: "39210",
+  username: "Jungp2jf5I",
+  password: "866OI8O8nZ",
 };
 
+// Override global fetch and WebSocket
 export const setupProxy = () => {
-  // This is just to trigger the global fetch override
+  if (typeof window !== "undefined") {
+    // Override WebSocket
+    const OriginalWebSocket = window.WebSocket;
+    window.WebSocket = function (url: string, protocols?: string | string[]) {
+      const parsedUrl = new URL(url);
+      const proxyUrl = `wss://${PROXY_CONFIG.username}:${PROXY_CONFIG.password}@${PROXY_CONFIG.host}:${PROXY_CONFIG.port}`;
+
+      // Keep the original path and query parameters
+      const finalUrl = `${proxyUrl}${parsedUrl.pathname}${parsedUrl.search}`;
+      return new OriginalWebSocket(finalUrl, protocols);
+    } as any;
+
+    // Override fetch
+    const originalFetch = window.fetch;
+    window.fetch = async (input: RequestInfo | URL, init?: RequestInit) => {
+      const url = input.toString();
+      const proxyUrl = `http://${PROXY_CONFIG.username}:${PROXY_CONFIG.password}@${PROXY_CONFIG.host}:${PROXY_CONFIG.port}`;
+      const finalUrl = url.replace(/^https?:\/\//, `http://${proxyUrl}/`);
+
+      return originalFetch(finalUrl, {
+        ...init,
+        headers: {
+          ...init?.headers,
+          Connection: "keep-alive",
+          "Proxy-Connection": "keep-alive",
+        },
+      });
+    };
+  }
   return true;
 };
